654f89a03a3b73c01c031eb40f3bcee93b9d584a
CodeMD96 <codemd96@gmail.com>
2022-12-15 13:16:36 +0000

    changed back ebd url

--- src/services/config.service.js
+++ src/services/config.service.js
@@ -2,7 +2,7 @@ angular.module('app').factory('ConfigService', [function() {
 
     var service = {};
 
-    service.apiBase = 'http://localhost:8001/';
+    service.apiBase = 'https://quizapimtd-env-1.eba-462pvi8m.us-west-2.elasticbeanstalk.com/';
 
     return service;
 }]);
